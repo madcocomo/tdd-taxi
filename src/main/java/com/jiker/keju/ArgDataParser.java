0cd2b4541083ab25c2e17bb29bc347606ecfbc83
Vic Wu <madcocomo@gmail.com>
2020-02-27 16:43:43 +0800

    fix coverage error

--- src/main/java/com/jiker/keju/ArgDataParser.java
+++ src/main/java/com/jiker/keju/ArgDataParser.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TestDataParser {
+public class ArgDataParser {
 
     public static final Pattern PATTERN = Pattern.compile("^(.+)公里,等待(.+)分钟$");
 
--- src/test/java/com/jiker/keju/ArgDataParserTest.java
+++ src/test/java/com/jiker/keju/ArgDataParserTest.java
@@ -7,20 +7,17 @@ import java.math.BigDecimal;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
-public class TestDataParserTest {
-
-    public static final double DELTA = 0.0001;
-
+public class TestDataParserArg {
     @Test
     public void should_get_int_distance_and_waiting_from_string() {
-        TestData testData = TestDataParser.parse("1公里,等待0分钟");
+        TestData testData = ArgDataParser.parse("1公里,等待0分钟");
         assertEquals(BigDecimal.valueOf(1), testData.getDistance());
         assertEquals(BigDecimal.valueOf(0), testData.getWaiting());
     }
 
     @Test
     public void should_get_float_distance_and_waiting_from_string() {
-        TestData testData = TestDataParser.parse("1.5公里,等待0.5分钟");
+        TestData testData = ArgDataParser.parse("1.5公里,等待0.5分钟");
         assertEquals(BigDecimal.valueOf(1.5), testData.getDistance());
         assertEquals(BigDecimal.valueOf(0.5), testData.getWaiting());
     }
@@ -28,7 +25,7 @@ public class TestDataParserTest {
     @Test
     public void should_throw_exception_if_not_in_right_format() {
         assertThrows(IllegalArgumentException.class, () -> {
-            TestDataParser.parse("3公,等5分");
+            ArgDataParser.parse("3公,等5分");
         });
     }
 }
